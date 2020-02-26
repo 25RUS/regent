6598fde50f953418b23609464b0358f7e9f981dc
25rus <adnet_9@live.com>
2020-02-26 21:06:15 +1000

    rs232-ok;+log

---
+++ Regent/Regent/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Regent
+{
+    class Logger
+    {
+        public async void RecordToLog(string logMessage, int target)
+        {
+            //StorageFolder appFolder = ApplicationData.Current.LocalFolder;
+            string outFile = "";
+
+            switch (target)
+            {
+                case 0:
+                    {
+                        outFile = "smmrp.log";
+                        break;
+                    }
+                case 1:
+                    {
+                        outFile = "error.log";
+                        break;
+                    }
+            }
+
+            try
+            {
+                DateTime D = DateTime.Now;
+                string temp = D.ToString() + " scanner_module: " + logMessage + Environment.NewLine;
+                temp.Replace("\n", " ");
+                //StorageFile logFile = await appFolder.CreateFileAsync(outFile, CreationCollisionOption.OpenIfExists);
+                //await FileIO.AppendTextAsync(logFile, temp);
+            }
+            catch
+            {
+                ;
+            }
+        }
+    }
+}
---
+++ Regent/Regent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace Regent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            RS232.Init pI = new RS232.Init();
+            pI.AddPorts();
+
+            //Console.WriteLine(RS232.Ports.portName[0]);
+        }
+    }
+}
